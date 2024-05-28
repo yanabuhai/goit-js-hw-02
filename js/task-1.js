162547a61ebdf2003228deff7a3d95c55726a6c6
Yana Buhai <buhaiyanaol@gmail.com>
2024-05-28 19:10:30 +0300

    add-all-task

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    const (totalPrice) = quantity * pricePerDroid;
-    if (totalOrder > customerCredits) {
-        return `"Insufficient funds!"`;
-    } else {
-        "You ordered ${quantity} droids worth ${totalPrice} credits!"
-    };
+  const totalPrice = quantity * pricePerDroid;
+  if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
